1101aa9d9988669662da393e00d667dcda9c5ab9
ranaelmalah <rana.elmallah@remotecoder.org>
2025-09-22 18:01:18 +0300

    show error messages and change style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,26 +11,40 @@ import { RouterLink,Router } from "@angular/router";
 })
 export class LoginComponent {
 
-  
+  emailErrorMessage:string='';
+  passwordErrorMessage:string='';
+
   constructor(private authservices:AuthService, private router:Router){}
 
 
   loginForm:FormGroup=new FormGroup({
     email: new FormControl("",[Validators.email,Validators.required]) ,
-    password:new FormControl("" ,[Validators.required])
+    password:new FormControl("" ,[Validators.required,Validators.minLength(6)])
   })
 
   SubmitionLoginForm(){
   const {email,password}=this.loginForm.value;
 
   this.authservices.login(email , password).then(userCredential =>{
+    this.emailErrorMessage=''
+    this.passwordErrorMessage=''
     console.log("sucess")
-  }).catch(error=>{
-    console.error("faild",error)
-  })
-  if(this.loginForm.valid){
     this.router.navigate(['/home']);
-    
-  }
+
+  }).catch(error=>{
+      if(error.code==="auth/invalid-email"){
+      this.emailErrorMessage="Invalid email format"
+    }
+    else if(error.code==="auth/invalid-credential"){
+      this.passwordErrorMessage="Wrong Email Or Password"
+    }
+    else if (error.code==="auth/weak-password"){
+      this.passwordErrorMessage="Password must be at least 6 characters"
+    }
+    console.error("faild",error)
+
+
+  })
+
 }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {ReactiveFormsModule,FormBuilder, FormGroup,FormControl, Validators}from '@angular/forms'
 import {AuthService} from '../services/auth.service';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-signup',
@@ -11,7 +11,10 @@ import { RouterLink } from "@angular/router";
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-  constructor(private AuthService:AuthService){}
+  emailErrorMessage:string='';
+  passwordErrorMessage:string='';
+
+  constructor(private AuthService:AuthService ,private router:Router){}
   signUpForm:FormGroup=new FormGroup({
     email:new FormControl("",[Validators.required,Validators.email]),
     password:new FormControl("",[Validators.required ,Validators.minLength(6)])
@@ -20,9 +23,22 @@ export class SignupComponent {
 SubmitionSignUpForm(){
   const {email,password}=this.signUpForm.value;
   this.AuthService.signUp(email,password).then(userCredential =>{
+    this.emailErrorMessage='';
+   this.passwordErrorMessage='';
+this.router.navigate(['/home'])
+
     console.log("sign sucess")
   }).catch(error=>{
+    if(error.code==="auth/invalid-email"){
+      this.emailErrorMessage="Invalid email format"
+    }else if(error.code==="auth/email-already-in-use"){
+      this.emailErrorMessage="This email is already registered"
+    } else if (error.code==="auth/weak-password"){
+      this.passwordErrorMessage="Password must be at least 6 characters"
+    }
+
     console.error("error",error)
   })
+  
 }
 }
